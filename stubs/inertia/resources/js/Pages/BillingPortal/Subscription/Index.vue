b814dbabfa0bc292cb53b89777ac6e16ba4dfd2c
Alex Renoki <rennokki@gmail.com>
2021-05-15 19:42:51 +0300

    wrapping

--- stubs/inertia/resources/js/Pages/BillingPortal/Subscription/Index.vue
+++ stubs/inertia/resources/js/Pages/BillingPortal/Subscription/Index.vue
@@ -2,27 +2,29 @@
     <billing-portal-layout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Billing Portal: Subscription
+                Billing Portal: Subscriptions
             </h2>
         </template>
 
-        <div class="grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-            <div
-                v-for="plan in plans"
-                :key="`plan-${plan.id}`"
-                class="w-full"
-            >
-                <plan-slide
-                    :plan="plan"
-                    :active="currentPlan && plan.id === currentPlan.id"
-                    :currentPlan="currentPlan"
-                    :features="plan.features"
-                    :recurring="recurring"
-                    :cancelled="cancelled"
-                    :onGracePeriod="onGracePeriod"
-                    :endingDate="endingDate"
-                    :disable="false"
-                />
+        <div class="space-y-6">
+            <div class="grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+                <div
+                    v-for="plan in plans"
+                    :key="`plan-${plan.id}`"
+                    class="w-full flex"
+                >
+                    <plan-slide
+                        :plan="plan"
+                        :active="currentPlan && plan.id === currentPlan.id"
+                        :currentPlan="currentPlan"
+                        :features="plan.features"
+                        :recurring="recurring"
+                        :cancelled="cancelled"
+                        :onGracePeriod="onGracePeriod"
+                        :endingDate="endingDate"
+                        :disable="false"
+                    />
+                </div>
             </div>
         </div>
     </billing-portal-layout>
